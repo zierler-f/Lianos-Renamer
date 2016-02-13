efabb16fb02c23de52616bbe4abd93aa42b82c47
Florian Zierler <zie15087@spengergasse.at>
2016-02-13 12:49:21 +0100

    Add tests

--- src/test/java/at/zierler/privat/test/TestLianosRenamer.java
+++ src/test/java/at/zierler/privat/test/TestLianosRenamer.java
@@ -6,6 +6,7 @@ import at.zierler.privat.exceptions.LianosRenamerException;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 public class TestLianosRenamer {
 
     FileHandler fileHandler;
+    ByteArrayInputStream in;
+
     @Rule
     public TemporaryFolder folder= new TemporaryFolder();
 
@@ -76,6 +79,22 @@ public class TestLianosRenamer {
         Assert.assertEquals(createdFile1,resultList.get(0));
         Assert.assertNotEquals(createdFile2,resultList.get(1));
         Assert.assertEquals("Pretty Little Liars - S04E12 - Now You See Me, Now You Don't.mkv",resultList.get(1).getName());
+    }
+
+    @Test
+    public void testWith2VideoFilesOfSameShowInOneFolder() throws IOException, LianosRenamerException {
+        LianosFile createdFolder = new LianosFile(folder.newFolder("test-folder"));
+        LianosFile createdFile1 = new LianosFile(folder.newFile("test-folder/second.chance.s01e01.mkv"));
+        LianosFile createdFile2 = new LianosFile(folder.newFile("test-folder/second.chance.s01e03.mp4"));
+        in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+        ArrayList<LianosFile> resultList = fileHandler.handle(createdFolder);
+        Assert.assertFalse(resultList.isEmpty());
+        Assert.assertEquals(2,resultList.size());
+        Assert.assertNotEquals(createdFile1,resultList.get(0));
+        Assert.assertNotEquals(createdFile2,resultList.get(1));
+        Assert.assertEquals("Second Chance - S01E01 - Suitable Donor.mkv",resultList.get(1).getName());
+        Assert.assertEquals("Second Chance - S01E03 - From Darkness, the Sun.mp4",resultList.get(0).getName());
     }
 
 }
