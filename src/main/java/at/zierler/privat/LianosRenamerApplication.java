7cf34ff957e179388233735105590e0698415682
Florian Zierler <zierler25@gmail.com>
2016-11-05 17:10:40 +0100

    Initial commit of Spring project

---
+++ src/main/java/at/zierler/privat/LianosRenamerApplication.java
@@ -0,0 +1,12 @@
+package at.zierler.privat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianosRenamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LianosRenamerApplication.class, args);
+	}
+}
