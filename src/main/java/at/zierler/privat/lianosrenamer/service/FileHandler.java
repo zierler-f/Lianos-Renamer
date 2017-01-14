40183b59a33b6d7084384950ab21617aa3752a92
Florian Zierler <zierler25@gmail.com>
2017-01-14 22:27:12 +0100

    Refactoring

--- src/main/java/at/zierler/privat/lianosrenamer/service/FileHandler.java
+++ src/main/java/at/zierler/privat/lianosrenamer/service/FileHandler.java
@@ -8,30 +8,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FileHandler {
 
+    List<String> videoExtensions = Arrays.asList("mp4", "mkv", "wmv", "flv");
+
     public List<Path> getListOfVideoFilesByListOfFiles(List<File> inputFiles) throws LianosRenamerException {
         List<Path> videoFiles = new ArrayList<>();
-        for(File f : inputFiles){
+        for (File f : inputFiles) {
             List<Path> dirFiles;
             try {
                 dirFiles = Files.walk(Paths.get(f.getPath())).filter(this::isVideoFile).collect(Collectors.toList());
             } catch (IOException e) {
-                throw new LianosRenamerException("There was a problem while walking files.",e);
+                throw new LianosRenamerException("There was a problem while walking files.", e);
             }
             videoFiles.addAll(dirFiles);
         }
         return videoFiles;
     }
 
-    private boolean isVideoFile(Path path) throws RuntimeException {
+    private boolean isVideoFile(Path path) {
         String[] tmpSplit = path.getFileName().toString().split("\\.");
-        String extension = tmpSplit[tmpSplit.length-1];
-        return extension.equals("mp4") || extension.equals("mkv") || extension.equals("wmv");
+        String extension = tmpSplit[tmpSplit.length - 1];
+        return videoExtensions.contains(extension);
     }
 
 }
--- src/test/java/at/zierler/privat/lianosrenamer/service/FileHandlerTest.java
+++ src/test/java/at/zierler/privat/lianosrenamer/service/FileHandlerTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 
-public class FileHandlerTest extends Assert{
+public class FileHandlerTest extends Assert {
 
     @Rule
     public TemporaryFolder testDir = new TemporaryFolder();
@@ -23,7 +23,7 @@ public class FileHandlerTest extends Assert{
     private FileHandler fileHandler;
 
     @Before
-    public void setup(){
+    public void setup() {
         fileHandler = new FileHandler();
     }
 
@@ -32,22 +32,22 @@ public class FileHandlerTest extends Assert{
         List<File> inputFiles = new ArrayList<>();
         File dir1 = testDir.newFolder("dir1");
         File videoFile1 = testDir.newFile("test.wmv");
-        File videoFile2 = new File(dir1,"test.mkv");
-        File normalFile1 = new File(dir1,"test.mp3");
+        File videoFile2 = new File(dir1, "test.mkv");
+        File normalFile1 = new File(dir1, "test.mp3");
         assertTrue(videoFile2.createNewFile());
         assertTrue(normalFile1.createNewFile());
-        File subdir1 = new File(dir1,"subdir1");
+        File subdir1 = new File(dir1, "subdir1");
         assertTrue(subdir1.mkdir());
-        File normalFile2 = new File(subdir1,"test.txt");
+        File normalFile2 = new File(subdir1, "test.txt");
         assertTrue(normalFile2.createNewFile());
-        File subdir2 = new File(subdir1,"subdir2");
+        File subdir2 = new File(subdir1, "subdir2");
         assertTrue(subdir2.mkdir());
-        File videoFile3 = new File(subdir2,"test.mp4");
+        File videoFile3 = new File(subdir2, "test.mp4");
         assertTrue(videoFile3.createNewFile());
         inputFiles.add(videoFile1);
         inputFiles.add(dir1);
         List<Path> resultFiles = fileHandler.getListOfVideoFilesByListOfFiles(inputFiles);
-        assertThat(resultFiles.size(),is(3));
+        assertThat(resultFiles.size(), is(3));
         assertTrue(resultFiles.contains(videoFile1.toPath()));
         assertTrue(resultFiles.contains(videoFile2.toPath()));
         assertTrue(resultFiles.contains(videoFile3.toPath()));
